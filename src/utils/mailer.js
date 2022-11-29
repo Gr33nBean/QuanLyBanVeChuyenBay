f77347ce28d4c68f6ff338e3c5ad73c7a3516cb1
TriTran951 <thanhtritran951@gmail.com>
2022-11-29 15:31:49 +0700

    Add sendMail

---
+++ src/utils/mailer.js
@@ -0,0 +1,32 @@
+const nodeMailer = require('nodemailer');
+import mailConfig from '../config/mail.config';
+const path = require('path');
+require('dotenv/config');
+
+let sendMail = async (to, subject, htmlContent, filename) => {
+    let transporter = nodeMailer.createTransport({
+        host: mailConfig.HOST,
+        port: mailConfig.PORT,
+        secure: false, // true for 465, false for other ports
+        auth: {
+            user: mailConfig.USERNAME, // generated ethereal user
+            pass: mailConfig.PASSWORD, // generated ethereal password
+        },
+    });
+
+    let pathAttach = path.join(__dirname, '../public/temp/' + filename);
+
+    let options = {
+        from: mailConfig.FROM_ADDRESS,
+        to: to,
+        subject: subject,
+        html: htmlContent,
+        attachments: [{ path: pathAttach }],
+    };
+
+    return transporter.sendMail(options);
+};
+
+module.exports = {
+    sendMail: sendMail,
+};
